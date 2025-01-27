d3c9caf18b23f7b49be6a872dcdc5ca02b312201
Luis Mdz <luis3mendez6@gmailcom>
2025-01-27 12:11:07 -0600

    Function 2 added

--- r4_pyfile_1.py
+++ r4_pyfile_1.py
@@ -5,3 +5,8 @@ def function1_rama4():
     """First function for rama 4"""
 
     print("Hello from rama4")
+
+def function2_rama4():
+    """Function dos for rama 4"""
+
+    print("Hello im functio2")
